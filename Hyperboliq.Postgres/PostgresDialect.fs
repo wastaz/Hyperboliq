19c5d514e54f346600955db1fe200edaf14bff8b
Fredrik Forssen <fredde@grodslok.net>
2015-09-21 00:29:50 +0200

    Added basic project for postgres dialect

---
+++ Hyperboliq.Postgres/PostgresDialect.fs
@@ -0,0 +1,13 @@
+﻿namespace Hyperboliq.Dialects
+
+open Hyperboliq
+open System.Data
+open System.Data.SqlClient
+open Npgsql
+
+type public PostgreSql private () =
+    static member private _dialect = lazy(new PostgreSql())
+    static member Dialect with get() = PostgreSql._dialect.Value
+    interface ISqlDialect with
+        member x.QuoteColumnName colname = @"""" + colname + @""""
+        member x.CreateConnection connectionString = new NpgsqlConnection(connectionString) :> IDbConnection
