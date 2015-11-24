507faf43c83552434e1e49a8b6d7a436c2331aa3
Fredrik Forssen <fredde@grodslok.net>
2015-11-24 15:43:12 +0100

    Extracted groupby test results

--- Hyperbolic.Tests/TokenGeneration/SimpleGroupByTests.cs
+++ Hyperbolic.Tests/TokenGeneration/SimpleGroupByTests.cs
@@ -1,11 +1,6 @@
 ﻿using Xunit;
 using Hyperboliq.Domain;
 using Hyperboliq.Tests.TokenGeneration;
-using S = Hyperboliq.Tests.SqlStreamExtensions;
-using AggregateType = Hyperboliq.Domain.AST.AggregateType;
-using BinaryOperation = Hyperboliq.Domain.AST.BinaryOperation;
-using JoinType = Hyperboliq.Domain.AST.JoinType;
-using ValueNode = Hyperboliq.Domain.AST.ValueNode;
 
 namespace Hyperboliq.Tests
 {
@@ -15,177 +10,59 @@ namespace Hyperboliq.Tests
         [Fact]
         public void ItShouldBePossibleToGroupByASingleColumn()
         {
-            var expr = Select.Column<Person>(p => new { p.Name, MaxAge = Sql.Max(p.Age) })
+            var expr = Select.Column<Person>(p => new { p.Name, maxAge = Sql.Max(p.Age) })
                              .From<Person>()
                              .GroupBy<Person>(p => p.Name);
             var result = expr.ToSqlExpression();
-
-            var expected =
-                S.SelectNode(
-                    S.Select(
-                        S.Col<Person>("Name"), 
-                        S.AliasedCol<Person>(
-                            AggregateType.Max,
-                            S.Col<Person>("Age"),
-                            "MaxAge")),
-                    S.From<Person>(),
-                    groupBy: S.GroupBy(S.Col<Person>("Name"))
-                    );
-
-            Assert.Equal(expected, result);
+            Assert.Equal(TokenGeneration_SimpleGroupByTests_Results.groupBySingleColumnExpression, result);
         }
 
         [Fact]
         public void ItShouldBePossibleToGroupByMultipleColumns()
         {
-            var expr = Select.Column<Person>(p => new { p.Name, p.LivesAtHouseId, MinAge = Sql.Min(p.Age) })
+            var expr = Select.Column<Person>(p => new { p.Name, p.LivesAtHouseId, minAge = Sql.Min(p.Age) })
                              .From<Person>()
                              .GroupBy<Person>(p => new { p.Name, p.LivesAtHouseId });
             var result = expr.ToSqlExpression();
-
-            var expected =
-                S.SelectNode(
-                    S.Select(
-                        S.Col<Person>("Name"), 
-                        S.Col<Person>("LivesAtHouseId"), 
-                        S.AliasedCol<Person>(
-                            AggregateType.Min,
-                            S.Col<Person>("Age"),
-                            "MinAge")),
-                    S.From<Person>(),
-                    groupBy: S.GroupBy(S.Col<Person>("Name"), S.Col<Person>("LivesAtHouseId"))
-                    );
-
-            Assert.Equal(expected, result);
+            Assert.Equal(TokenGeneration_SimpleGroupByTests_Results.groupByMultipleColumnsExpression, result);
         }
-
-        [Fact]
-        public void ItShouldBePossibleToGroupByColumnsFromSeveralTables()
-        {
-            var expr = Select.Column<Person>(p => new { p.Age })
-                             .Column<Car>(c => new { c.Brand, NumberOfPersons = Sql.Count() })
-                             .From<Person>()
-                             .InnerJoin<Person, Car>((p, c) => p.Id == c.DriverId)
-                             .GroupBy<Person>(p => p.Age).ThenBy<Car>(c => c.Brand);
-            var result = expr.ToSqlExpression();
-
-            var expected =
-                S.SelectNode(
-                    S.Select(
-                        S.Col<Car>("Brand"), 
-                        S.AliasedCol<Person>(
-                            AggregateType.Count,
-                            ValueNode.NullValue,
-                            "NumberOfPersons"),
-                        S.Col<Person>("Age")),
-                    S.From<Person>(
-                        S.Join<Person, Car>(JoinType.InnerJoin, S.BinExp(S.Col<Person>("Id"), BinaryOperation.Equal, S.Col<Car>("DriverId")))),
-                    groupBy: S.GroupBy(S.Col<Person>("Age"), S.Col<Car>("Brand"))
-                    );
-
-            Assert.Equal(expected, result);
-        }
-
+        
         [Fact]
         public void ItShouldBePossibleToGroupByColumnsFromMultipleTables()
         {
-            var expr = Select.Column<Person>(p => new { p.Name, AverageAge = Sql.Avg(p.Age) })
-                             .Column<Car>(c => new { c.Brand, MinAge = Sql.Min(c.Age) })
+            var expr = Select.Column<Person>(p => new { p.Name, averageAge = Sql.Avg(p.Age) })
+                             .Column<Car>(c => new { c.Brand, minAge = Sql.Min(c.Age) })
                              .From<Person>()
                              .InnerJoin<Person, Car>((p, c) => p.Id == c.DriverId)
                              .GroupBy<Person>(p => p.Name)
                              .ThenBy<Car>(c => c.Brand);
             var result = expr.ToSqlExpression();
-
-            var expected =
-                S.SelectNode(
-                    S.Select(
-                        S.Col<Car>("Brand"),
-                        S.AliasedCol<Person>(
-                            AggregateType.Min,
-                            S.Col<Car>("Age"), 
-                            "MinAge"),
-                        S.Col<Person>("Name"),
-                        S.AliasedCol<Person>(
-                            AggregateType.Avg,
-                            S.Col<Person>("Age"),
-                            "AverageAge")),
-                    S.From<Person>(
-                        S.Join<Person, Car>(JoinType.InnerJoin, S.BinExp(S.Col<Person>("Id"), BinaryOperation.Equal, S.Col<Car>("DriverId")))),
-                    groupBy: S.GroupBy(S.Col<Person>("Name"), S.Col<Car>("Brand"))
-                    );
-
-            Assert.Equal(expected, result);
+            Assert.Equal(TokenGeneration_SimpleGroupByTests_Results.groupByColumnsFromMultipleTablesExpression, result);
         }
 
         [Fact]
         public void ItShouldBePossibleToUseASingleHavingExpression()
         {
-            var expr = Select.Column<Person>(p => new { p.Name, AverageAge = Sql.Avg(p.Age) })
+            var expr = Select.Column<Person>(p => new { p.Name, averageAge = Sql.Avg(p.Age) })
                              .From<Person>()
                              .GroupBy<Person>(p => p.Name)
                              .Having<Person>(p => Sql.Avg(p.Age) > 42);
             var result = expr.ToSqlExpression();
-
-            var expected =
-                S.SelectNode(
-                    S.Select(
-                        S.Col<Person>("Name"), 
-                        S.AliasedCol<Person>(
-                            AggregateType.Avg, 
-                            S.Col<Person>("Age"),
-                            "AverageAge")),
-                    S.From<Person>(),
-                    groupBy: S.GroupBy(
-                        new[] { S.Col<Person>("Name") }, 
-                        S.And(S.BinExp(S.Aggregate(AggregateType.Avg, S.Col<Person>("Age")), BinaryOperation.GreaterThan, S.Const(42))))
-                    );
-
-            Assert.Equal(expected, result);
+            Assert.Equal(TokenGeneration_SimpleGroupByTests_Results.groupByWithSingleHavingExpression, result);
         }
 
         [Fact]
         public void ItShouldBePossibleToUseMultipleHavingExpressions()
         {
-            var expr = Select.Column<Person>(p => new { p.Name, AverageAge = Sql.Avg(p.Age) })
-                             .Column<Car>(c => new { c.Brand, MinAge = Sql.Min(c.Age) })
+            var expr = Select.Column<Person>(p => new { p.Name, averageAge = Sql.Avg(p.Age) })
+                             .Column<Car>(c => new { c.Brand, minAge = Sql.Min(c.Age) })
                              .From<Person>()
                              .InnerJoin<Person, Car>((p, c) => p.Id == c.DriverId)
                              .GroupBy<Person>(p => p.Name).ThenBy<Car>(c => c.Brand)
                              .Having<Person>(p => Sql.Avg(p.Age) > 42)
                              .And<Car>(c => Sql.Min(c.Age) > 2);
             var result = expr.ToSqlExpression();
-
-            var expected =
-                S.SelectNode(
-                    S.Select(
-                        S.Col<Car>("Brand"),
-                        S.AliasedCol<Person>(
-                            AggregateType.Min, 
-                            S.Col<Car>("Age"),
-                            "MinAge"),
-                        S.Col<Person>("Name"),
-                        S.AliasedCol<Person>(
-                            AggregateType.Avg, 
-                            S.Col<Person>("Age"),
-                            "AverageAge")),
-                    S.From<Person>(
-                        S.Join<Person, Car>(JoinType.InnerJoin, S.BinExp(S.Col<Person>("Id"), BinaryOperation.Equal, S.Col<Car>("DriverId")))),
-                    groupBy: S.GroupBy(
-                        new[] { S.Col<Person>("Name"), S.Col<Car>("Brand") },
-                        S.And(
-                            S.BinExp(
-                                S.Aggregate(AggregateType.Min, S.Col<Car>("Age")),
-                                BinaryOperation.GreaterThan,
-                                S.Const(2))),
-                        S.And(
-                            S.BinExp(
-                                S.Aggregate(AggregateType.Avg, S.Col<Person>("Age")),
-                                BinaryOperation.GreaterThan,
-                                S.Const(42)))
-                        ));
-
-            Assert.Equal(expected, result);
+            Assert.Equal(TokenGeneration_SimpleGroupByTests_Results.groupByWithMultipleHavingExpression, result);
         }
     }
 }
---
+++ Hyperboliq.Tests.Common/TokenGeneration_SimpleGroupByTests.fs
@@ -0,0 +1,99 @@
+﻿namespace Hyperboliq.Tests.TokenGeneration
+
+module TokenGeneration_SimpleGroupByTests_Results =
+  open Hyperboliq
+  open Hyperboliq.Domain.AST
+  open Hyperboliq.Domain
+
+  let personRef = TableIdentifier<Person>()
+  let pref = personRef.Reference :> ITableReference
+
+  let carRef = TableIdentifier<Car>()
+  let cref = carRef.Reference :> ITableReference
+
+  let groupBySingleColumnExpression =
+    { TestHelpers.EmptySelect with 
+        Select = { IsDistinct = false
+                   Values = [ ValueNode.Column("Name", typeof<string>, pref)
+                              ValueNode.NamedColumn({ Alias = "maxAge"
+                                                      Column = ValueNode.Aggregate(AggregateType.Max, ValueNode.Column("Age", typeof<int>, pref)) }) ] } 
+        From = { Tables = [ personRef ]; Joins = [] }
+        GroupBy = { Clauses = [ ValueNode.Column("Name", typeof<string>, pref) ]
+                    Having = [] } |> Some
+    } |> TestHelpers.ToPlainSelect
+
+  let groupByMultipleColumnsExpression =
+    { TestHelpers.EmptySelect with
+        Select =  { IsDistinct = false
+                    Values = [ ValueNode.Column("Name", typeof<string>, pref)
+                               ValueNode.Column("LivesAtHouseId", typeof<int>, pref)
+                               ValueNode.NamedColumn({ Alias = "minAge"
+                                                       Column = ValueNode.Aggregate(AggregateType.Min, ValueNode.Column("Age", typeof<int>, pref)) }) ] } 
+        From = { Tables = [ personRef ]; Joins = [] }
+        GroupBy = { Clauses = [ ValueNode.Column("Name", typeof<string>, pref)
+                                ValueNode.Column("LivesAtHouseId", typeof<int>, pref) ]
+                    Having = [] } |> Some
+    } |> TestHelpers.ToPlainSelect
+
+  let groupByColumnsFromMultipleTablesExpression =
+    { TestHelpers.EmptySelect with
+        Select =  { IsDistinct = false
+                    Values = [ ValueNode.Column("Brand", typeof<string>, cref)
+                               ValueNode.NamedColumn({ Alias = "minAge"
+                                                       Column = ValueNode.Aggregate(AggregateType.Min, ValueNode.Column("Age", typeof<int>, cref)) })
+                               ValueNode.Column("Name", typeof<string>, pref)
+                               ValueNode.NamedColumn({ Alias = "averageAge"
+                                                       Column = ValueNode.Aggregate(AggregateType.Avg, ValueNode.Column("Age", typeof<int>, pref)) }) ] } 
+        From = { Tables = [ personRef ]
+                 Joins = [ { SourceTables = [ personRef ]
+                             TargetTable = carRef
+                             Type = JoinType.InnerJoin
+                             Condition = { Operation = BinaryOperation.Equal
+                                           Lhs = ValueNode.Column("Id", typeof<int>, pref)
+                                           Rhs = ValueNode.Column("DriverId", typeof<int>, cref) } |> ValueNode.BinaryExpression |> Some } ] }
+        GroupBy = { Clauses = [ ValueNode.Column("Name", typeof<string>, pref)
+                                ValueNode.Column("Brand", typeof<string>, cref) ] 
+                    Having = [] } |> Some
+    } |> TestHelpers.ToPlainSelect
+
+  let groupByWithSingleHavingExpression =
+    { TestHelpers.EmptySelect with
+        Select = { IsDistinct = false
+                   Values = [ ValueNode.Column("Name", typeof<string>, pref)
+                              ValueNode.NamedColumn({ Alias = "averageAge"
+                                                      Column = ValueNode.Aggregate(AggregateType.Avg, ValueNode.Column("Age", typeof<int>, pref)) }) ] } 
+        From = { Tables = [ personRef ]; Joins = [] }
+        GroupBy = { Clauses = [ ValueNode.Column("Name", typeof<string>, pref) ]
+                    Having = [ { Combinator = ExpressionCombinatorType.And
+                                 Expression = { Operation = BinaryOperation.GreaterThan
+                                                Lhs = ValueNode.Aggregate(AggregateType.Avg, ValueNode.Column("Age", typeof<int>, pref))
+                                                Rhs = ValueNode.Constant("42") } |> ValueNode.BinaryExpression }  ] } |> Some
+    } |> TestHelpers.ToPlainSelect
+
+  let groupByWithMultipleHavingExpression =
+    { TestHelpers.EmptySelect with 
+        Select = { IsDistinct = false
+                   Values = [ ValueNode.Column("Brand", typeof<string>, cref)
+                              ValueNode.NamedColumn({ Alias = "minAge"
+                                                      Column = ValueNode.Aggregate(AggregateType.Min, ValueNode.Column("Age", typeof<int>, cref)) })
+                              ValueNode.Column("Name", typeof<string>, pref)
+                              ValueNode.NamedColumn({ Alias = "averageAge"
+                                                      Column = ValueNode.Aggregate(AggregateType.Avg, ValueNode.Column("Age", typeof<int>, pref)) }) ] }
+        From = { Tables = [ personRef ]
+                 Joins = [ { SourceTables = [ personRef ]
+                             TargetTable = carRef
+                             Type = JoinType.InnerJoin
+                             Condition = { Operation = BinaryOperation.Equal
+                                           Lhs = ValueNode.Column("Id", typeof<int>, pref)
+                                           Rhs = ValueNode.Column("DriverId", typeof<int>, cref) } |> ValueNode.BinaryExpression |> Some } ] }
+        GroupBy = { Clauses = [ ValueNode.Column("Name", typeof<string>, pref)
+                                ValueNode.Column("Brand", typeof<string>, cref) ]
+                    Having = [ { Combinator = ExpressionCombinatorType.And 
+                                 Expression = { Operation = BinaryOperation.GreaterThan
+                                                Lhs = ValueNode.Aggregate(AggregateType.Min, ValueNode.Column("Age", typeof<int>, cref))
+                                                Rhs = ValueNode.Constant("2") } |> ValueNode.BinaryExpression }
+                               { Combinator = ExpressionCombinatorType.And
+                                 Expression = { Operation = BinaryOperation.GreaterThan
+                                                Lhs = ValueNode.Aggregate(AggregateType.Avg, ValueNode.Column("Age", typeof<int>, pref))
+                                                Rhs = ValueNode.Constant("42") } |> ValueNode.BinaryExpression } ] } |> Some
+    } |> TestHelpers.ToPlainSelect
