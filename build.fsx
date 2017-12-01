66d5807fffe76c6e8f8e11643711107872b064ae
Fredrik Forssén <fredrik.forssen@paynova.com>
2017-12-01 14:33:32 +0100

    Bump version

--- build.fsx
+++ build.fsx
@@ -8,7 +8,7 @@ open System
 let artifactsDir = "./artifacts/"
 let testArtifactsDir = "./testartifacts/"
 let packagingDir = "./packaging/"
-let version = "0.2.0"
+let version = "0.3.1"
 
 let testProjectFiles =
   [ "./Hyperboliq.Tests/Hyperboliq.Tests.csproj"
